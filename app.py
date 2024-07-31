6ead8dc7a8582183282fb1bff584ff75106824ef
techcity1985 <93732214+techcity1985@users.noreply.github.com>
2024-07-31 21:28:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,31 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Define functions for each operation
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    operation = request.form['operation']
+    
+    if operation == 'add':
+        result = add(num1, num2)
+    elif operation == 'subtract':
+        result is subtract(num1, num2)
+    elif operation == 'multiply':
+        result = multiply(num1, num2)
+    elif operation == 'divide':
+        result = divide(num1, num2)
+    elif operation == 'modulus':
+        result = modulus(num1, num2)
+    elif operation == 'exponentiate':
+        result = exponentiate(num1, num2)
+    
+    return render_template('index.html', result=result)
+
 def add(x, y):
     return x + y
 
@@ -21,33 +45,8 @@ def divide(x, y):
 def modulus(x, y):
     return x % y
 
-def exponential(x, y):
+def exponentiate(x, y):
     return x ** y
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    num1 = float(request.form['num1'])
-    num2 = float(request.form['num2'])
-    operation = request.form['operation']
-    
-    if operation == 'add':
-        result = add(num1, num2)
-    elif operation == 'subtract':
-        result = subtract(num1, num2)
-    elif operation == 'multiply':
-        result = multiply(num1, num2)
-    elif operation == 'divide':
-        result = divide(num1, num2)
-    elif operation == 'modulus':
-        result = modulus(num1, num2)
-    elif operation == 'exponentiate':
-        result = exponential(num1, num2)
-    
-    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=False)
